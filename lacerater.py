e46e0f9ab017fdf4846863056eabc39e055ec0ea
Daniel Lombardo <kayshcache@github.com>
2019-07-04 11:24:19 +1000

    defined function for main()

--- lacerater.py
+++ lacerater.py
@@ -11,6 +11,32 @@ import requests
 import os
 import bs4
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='The website address to be scraped.')
+    parser.add_argument('--source', help='Print the source of the webpage.', action='store_true')
+    parser.add_argument('--text', help='Print all text from the webpage.', action='store_true')
+    parser.add_argument('--explore', help='Iterates over response object', action='store_true')
+    
+    cli_args = parser.parse_args()
+    url_obj = download_url(cli_args.url)
+    output = ''
+    
+    if cli_args.source:
+        output = parse_source(url_obj)
+
+    if cli_args.explore:
+        response_list = list(url_obj)
+        print(url_obj.headers)
+        # print(response_list)
+        # for k in response_list:
+        #     print(k,'\n')
+        
+    if cli_args.text:
+        output = strip_html(url_obj)
+
+    print(output)
+
 def download_url(url):
     '''
     Takes a URL as a string. Use requests to get an object of the response from the server.
@@ -40,27 +66,4 @@ def strip_html(url_obj):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('url', help='The website address to be scraped.')
-    parser.add_argument('--source', help='Print the source of the webpage.', action='store_true')
-    parser.add_argument('--text', help='Print all text from the webpage.', action='store_true')
-    parser.add_argument('--explore', help='Iterates over response object', action='store_true')
-    
-    cli_args = parser.parse_args()
-    url_obj = download_url(cli_args.url)
-    output = ''
-    
-    if cli_args.source:
-        output = parse_source(url_obj)
-
-    if cli_args.explore:
-        response_list = list(url_obj)
-        print(url_obj.headers)
-        # print(response_list)
-        # for k in response_list:
-        #     print(k,'\n')
-        
-    if cli_args.text:
-        output = strip_html(url_obj)
-
-    print(output)
+    main()
