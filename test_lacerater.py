097eee737766eef7f2d1e8fbf9c9ab990fe6740a
Daniel Lombardo <kayshcache@github.com>
2019-07-06 22:23:06 +1000

    made the tests

--- test_lacerater.py
+++ test_lacerater.py
@@ -1,4 +1,12 @@
-from lacerater import download_url
+import requests
+import os
+from lacerater import download_url, make_file
 
+URL_OBJ = download_url('http://www.coder-academy-ctf.site/')
 def test_download_url():
-    download_url('https://www.google.com')
+    assert isinstance(URL_OBJ, requests.models.Response) == True # Expect to create a requests class object from URL
+
+def test_make_file():
+    filename = make_file(URL_OBJ)
+    assert os.path.exists(filename) == True # Expect to create the file
+    os.remove(filename)
